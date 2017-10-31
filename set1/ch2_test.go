09341f33fe97a635e45a3edf3126dd9cd9e3a216
Josh Yotty <joshyotty@gmail.com>
2017-10-31 16:13:35 -0700

    set 1 challenge 2

---
+++ set1/ch2_test.go
@@ -0,0 +1,8 @@
+package set1
+
+import "fmt"
+
+func ExampleHexXOR() {
+	fmt.Println(hexXOR("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"))
+	// Output: 746865206b696420646f6e277420706c6179
+}
--- set1/lib.go
+++ set1/lib.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/hex"
+	//"fmt"
 	"log"
+	//"os"
 )
 
 func hexToB64(hs string) string {
@@ -18,4 +20,29 @@ func hexToB64(hs string) string {
 	encoder.Write(b)
 	encoder.Close()
 	return buf.String()
+}
+
+func hexXOR(a, b string) string {
+	if len(a) != len(b) {
+		log.Fatal("can't xor unequal length strings, pad")
+	}
+
+	ba, err := hex.DecodeString(a)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bb, err := hex.DecodeString(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	l := len(ba)
+	dest := make([]byte, l, l)
+
+	for i := 0; i < l; i++ {
+		dest[i] = ba[i] ^ bb[i]
+	}
+
+	return hex.EncodeToString(dest)
 }
